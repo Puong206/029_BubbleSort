1c9007623e2685913a86fd6f4ad3789fe0483a31
Puong206 <bagasrf206@gmail.com>
2024-03-07 21:05:49 +0700

    Prosedur Input

--- 029_BubbleSort/029_BubbleSort.cpp
+++ 029_BubbleSort/029_BubbleSort.cpp
@@ -5,3 +5,26 @@ using namespace std;
 
 int array[20];
 int n;
+
+void input() {
+
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\rArray dapat mempunyai maksimal 20 elemen.\n";              //output ke layar
+		}
+	}
+	cout << endl;                                    //output baris kosong
+	cout << "===============" << endl;               //output ke layar
+	cout << "Masukkan Elemen Array" << endl;         //output ke layar
+	cout << "===============" << endl;               //output ke layar
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> array[i];
+
+	}
+}
